257a97b506725d03e21f673d4c522052a37828c5
C C Nipun Das <nipundas02@gmail.com>
2023-05-19 21:29:59 +0530

    slider-half

---
+++ app-frontend/IntroSlider1.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {View,Text,StyleSheet} from 'react-native'
+import AppIntroSlider from "react-native-app-intro-slider";
+
+
+const slides=[
+    {
+        key:'one',
+        title:'Title 1',
+        text:'text sample is this an apple mango orange all at one place.'
+    }
+]
+
+
+
+
+export default class IntroSlider1 extends React.Component{
+    render(){
+        return(
+            <View style={StyleSheet.container}>
+                <View>
+                    <Text>skfbhksdfj</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        alignItems:'center',
+        flex:'1'
+    }
+})
